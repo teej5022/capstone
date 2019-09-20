a9dc69605f07b6581e49a1d835ddbf6df74c5e1f
teej5022 <tjschultz5022@gmail.com>
2019-09-20 11:12:48 -0500

    firebase changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import * as firebase from 'firebase';
 
-var firebaseConfig = {
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import { Navbar } from "./pages/navbar";
+import Lord from "./pages/lord";
+
+
+import "./styles.scss";
+
+const firebaseConfig = {
     apiKey: "AIzaSyAUh7nFchtMeQNV1hcngfT-NyWiwCQJujk",
     authDomain: "capstone-199aa.firebaseapp.com",
     databaseURL: "https://capstone-199aa.firebaseio.com",
@@ -13,17 +22,8 @@ var firebaseConfig = {
     appId: "1:596115894545:web:c406d00d41200889b61cb0"
 };
 
-firebase.initializeApp(firebaseConfig);
 
 
-import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import { Navbar } from "./pages/navbar";
-import Lord from "./pages/lord";
-
-
-import "./styles.scss";
 
 
 function Cap() {
