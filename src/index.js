1b9dd26705c27378b7cffb5930cdea933fa6f871
teej5022 <tjschultz5022@gmail.com>
2019-09-20 12:20:10 -0500

    heroku changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import * as firebase from 'firebase';
 
 import Home from "./pages/home";
 import About from "./pages/about";
